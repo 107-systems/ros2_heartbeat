18af832bafe16b643c48df88829c6d94ba8f2049
Alexander Entinger <consulting@lxrobotics.com>
2023-06-27 06:23:37 +0200

    Ensure that a heartbeat signal is sent both with a deadline and a liveliness lease duration.

--- include/ros2_heartbeat/Publisher.h
+++ include/ros2_heartbeat/Publisher.h
@@ -15,6 +15,7 @@
 #include <string>
 #include <memory>
 
+#include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <std_msgs/msg/u_int64.hpp>
@@ -36,21 +37,30 @@ public:
   typedef std::shared_ptr<Publisher> SharedPtr;
 
 
-  static SharedPtr create(rclcpp::Node & node_hdl, std::string const & heartbeat_topic, std::chrono::milliseconds const heartbeat_rate) {
-    return std::make_shared<Publisher>(node_hdl, heartbeat_topic, heartbeat_rate);
+  static SharedPtr create(
+    rclcpp::Node & node_hdl,
+    std::string const & heartbeat_topic,
+    std::chrono::milliseconds const heartbeat_rate,
+    std::chrono::milliseconds const heartbeat_deadline,
+    std::chrono::milliseconds const liveliness_lease_duration)
+  {
+    return std::make_shared<Publisher>(node_hdl, heartbeat_topic, heartbeat_rate, heartbeat_deadline, liveliness_lease_duration);
   }
 
 
   Publisher(
     rclcpp::Node & node_hdl,
     std::string const & heartbeat_topic,
-    std::chrono::milliseconds const heartbeat_rate
+    std::chrono::milliseconds const heartbeat_rate,
+    std::chrono::milliseconds const heartbeat_deadline,
+    std::chrono::milliseconds const liveliness_lease_duration
     );
 
 
 private:
   std::chrono::steady_clock::time_point _heartbeat_start;
 
+  rclcpp::QoS _heartbeat_qos_profile;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr _heartbeat_pub;
   rclcpp::TimerBase::SharedPtr _heartbeat_loop_timer;
 };
--- src/Publisher.cpp
+++ src/Publisher.cpp
@@ -24,20 +24,33 @@ namespace heartbeat
 Publisher::Publisher(
   rclcpp::Node & node_hdl,
   std::string const & heartbeat_topic,
-  std::chrono::milliseconds const heartbeat_rate)
+  std::chrono::milliseconds const heartbeat_rate,
+  std::chrono::milliseconds const heartbeat_deadline,
+  std::chrono::milliseconds const liveliness_lease_duration)
 : _heartbeat_start{std::chrono::steady_clock::now()}
+, _heartbeat_qos_profile
 {
-  _heartbeat_pub = node_hdl.create_publisher<std_msgs::msg::UInt64>(heartbeat_topic, 1);
+  rclcpp::KeepLast(1),
+  rmw_qos_profile_default
+}
+{
+  _heartbeat_qos_profile.deadline(heartbeat_deadline);
+  _heartbeat_qos_profile.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC);
+  _heartbeat_qos_profile.liveliness_lease_duration(liveliness_lease_duration);
+
+  _heartbeat_pub = node_hdl.create_publisher<std_msgs::msg::UInt64>(
+    heartbeat_topic,
+    _heartbeat_qos_profile
+    );
 
   _heartbeat_loop_timer = node_hdl.create_wall_timer(
     heartbeat_rate,
     [this]()
     {
+      auto const node_up_time = std::chrono::steady_clock::now() - _heartbeat_start;
+
       std_msgs::msg::UInt64 heartbeat_msg;
-
-      heartbeat_msg.data = std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::steady_clock::now() - _heartbeat_start).count();
-
+      heartbeat_msg.data = std::chrono::duration_cast<std::chrono::seconds>(node_up_time).count();
       _heartbeat_pub->publish(heartbeat_msg);
     });
 }
